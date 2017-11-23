0d85078183d3ff2dbe546927b2c92c3b10d366b8
sdp518 <[sdp518@york.ac.uk]>
2017-11-23 01:04:20 +0000

    Adding game title

--- desktop/src/com/sidmeier/campuschaos/desktop/DesktopLauncher.java
+++ desktop/src/com/sidmeier/campuschaos/desktop/DesktopLauncher.java
@@ -8,6 +8,7 @@ import com.sidmeier.campuschaos.utils.Constants;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Campus Chaos";
 		config.width = Constants.APP_WIDTH;
 		config.height = Constants.APP_HEIGHT;
 		new LwjglApplication(new CampusChaos(), config);

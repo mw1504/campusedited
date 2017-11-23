e5c3eb8ceba7195150bfedb887102583ac726840
sdp518 <[sdp518@york.ac.uk]>
2017-11-23 01:05:52 +0000

    Adding edge scrolling, cleanup & comments

--- core/src/com/sidmeier/campuschaos/CampusChaos.java
+++ core/src/com/sidmeier/campuschaos/CampusChaos.java
@@ -21,13 +21,11 @@ public class CampusChaos extends ApplicationAdapter {
     private SpriteBatch batch;
     private Sprite mapSprite;
 
-//    private float rotationSpeed;
-
-    // Defines mapSprite, camera and viewport
+    /**
+     * Defines mapSprite, camera and viewport
+     */
 	@Override
 	public void create () {
-//        rotationSpeed = 0.5f;
-
         batch = new SpriteBatch();
         mapSprite = new Sprite(new Texture(Gdx.files.internal("map_placeholder.jpg")));
         mapSprite.setPosition(0, 0);
@@ -40,7 +38,9 @@ public class CampusChaos extends ApplicationAdapter {
         cam.position.set(cam.viewportWidth/2,cam.viewportHeight/2,0);
 	}
 
-	// Updates camera position, clears screen and draws mapSprite
+    /**
+     * Updates camera position, clears screen and draws mapSprite
+     */
 	@Override
 	public void render () {
         handleInput();
@@ -55,8 +55,12 @@ public class CampusChaos extends ApplicationAdapter {
         batch.end();
 	}
 
-	// Handles user input to move camera, ensures camera doesn't leave world
+    /**
+     * Handles user input to move camera, ensures camera doesn't leave world
+     */
     private void handleInput() {
+        float camSpeed = 3;    // Camera speed in units per frame
+
         if (Gdx.input.isKeyPressed(Input.Keys.A)) {
             cam.zoom += 0.02;
         }
@@ -64,43 +68,65 @@ public class CampusChaos extends ApplicationAdapter {
             cam.zoom -= 0.02;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            cam.translate(-3, 0, 0);
+            cam.translate(-camSpeed, 0, 0);
         }
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            cam.translate(3, 0, 0);
+            cam.translate(camSpeed, 0, 0);
         }
         if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            cam.translate(0, -3, 0);
+            cam.translate(0, -camSpeed, 0);
         }
         if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            cam.translate(0, 3, 0);
+            cam.translate(0, camSpeed, 0);
         }
-//        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-//            cam.rotate(-rotationSpeed, 0, 0, 1);
-//        }
-//        if (Gdx.input.isKeyPressed(Input.Keys.E)) {
-//            cam.rotate(rotationSpeed, 0, 0, 1);
-//        }
 
+        // Edge scrolling
+        float edgePercent = 5;  // Percentage of APP edge that will cause scrolling on mouseover
+        float x = Gdx.input.getX();
+        float y = Gdx.input.getY();
+
+        float leftEdge = ((Constants.APP_WIDTH / 100) * edgePercent);
+        float rightEdge = Constants.APP_WIDTH - leftEdge;
+        float bottomEdge = ((Constants.APP_HEIGHT / 100) * edgePercent);
+        float topEdge = Constants.APP_HEIGHT - bottomEdge;
+
+        if (x <= leftEdge) {
+            cam.translate(-camSpeed, 0, 0);
+        } else if (x >= rightEdge) {
+            cam.translate(camSpeed, 0, 0);
+        }
+
+        if (y <= bottomEdge) {
+            cam.translate(0, camSpeed, 0);
+        } else if (y >= topEdge) {
+            cam.translate(0, -camSpeed, 0);
+        }
+
+        // Edge clamping
         cam.zoom = MathUtils.clamp(cam.zoom, 0.1f, Constants.WORLD_HEIGHT/cam.viewportWidth);
 
         float effectiveViewportWidth = cam.viewportWidth * cam.zoom;
         float effectiveViewportHeight = cam.viewportHeight * cam.zoom;
 
-        cam.position.x = MathUtils.clamp(cam.position.x, effectiveViewportWidth / 2f, Constants.WORLD_WIDTH - effectiveViewportWidth / 2f);
-        cam.position.y = MathUtils.clamp(cam.position.y, effectiveViewportHeight / 2f, Constants.WORLD_HEIGHT - effectiveViewportHeight / 2f);
+        cam.position.x = MathUtils.clamp(cam.position.x, effectiveViewportWidth / 2f, (Constants.WORLD_WIDTH - effectiveViewportWidth / 2f) - 1);
+        cam.position.y = MathUtils.clamp(cam.position.y, effectiveViewportHeight / 2f, (Constants.WORLD_HEIGHT - effectiveViewportHeight / 2f) - 1);
+
     }
 
-    // Handles camera update on resizing of game window
+    /**
+     * Handles camera update on resizing of game window
+     */
     @Override
     public void resize(int width, int height) {
         viewport.update(width,height);
         cam.position.set(cam.viewportWidth/2,cam.viewportHeight/2,0);
     }
 
-    // Memory management
+    /**
+     * Memory management
+     */
     @Override
 	public void dispose () {
-		mapSprite.getTexture().dispose();
+	    mapSprite.getTexture().dispose();
 	}
 }

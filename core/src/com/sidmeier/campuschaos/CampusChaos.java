b43b04a53e342b9572a3defd61220a35a2dc246b
sdp518 <[sdp518@york.ac.uk]>
2017-12-13 15:16:48 +0000

    Removing redundant imports

--- core/src/com/sidmeier/campuschaos/CampusChaos.java
+++ core/src/com/sidmeier/campuschaos/CampusChaos.java
@@ -5,21 +5,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.HexagonalTiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.sidmeier.campuschaos.utils.Constants;
 
 public class CampusChaos extends ApplicationAdapter {
 
--- core/src/com/sidmeier/campuschaos/utils/Constants.java
+++ core/src/com/sidmeier/campuschaos/utils/Constants.java
@@ -5,6 +5,8 @@ public class Constants {
     public static final int APP_WIDTH = 1200;
     public static final int APP_HEIGHT = 800;
 
+    //TODO Work out if still needed and implement
+
     public static final int WORLD_WIDTH = 1271;
     public static final int WORLD_HEIGHT = 716;
 }
